1bdcab6a93d43008e9a6c7af197c5d2c7063f32e
Kshitiz Vishwakarma <kshitiz.vishwakarma2003@gmail.com>
2026-07-04 15:33:23 +0530

    Add: average-waiting-time - submission-1

---
+++ Data Structures & Algorithms/average-waiting-time/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double averageWaitingTime(int[][] customers) {
+
+        long currentTime = 0;
+        long waitingTime = 0;
+
+        for (int[] customer : customers) {
+
+            int arrival = customer[0];
+            int cook = customer[1];
+
+            if (arrival > currentTime) {
+                currentTime = arrival;
+            }
+
+            currentTime += cook;
+
+            waitingTime += currentTime - arrival;
+        }
+
+        return (double) waitingTime / customers.length;
+    }
+}
